1053b7e32512f9d12556e96b39370b00aad47a2b
Wazabiii42 <fourcade.adri2@gmail.com>
2016-09-22 19:11:30 +0200

    ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,16 +1,27 @@
 #include "libft.h"
 
-void	*memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	int		*tmp;
-	unsigned int	i;
+	size_t	i;
+	char	*tmp_dest;
+	const char	*tmp_src;
 
 	i = 0;
-	while (i < n)
-	{
-		tmp[i] = src[i];
-		i++;
-	}
-	i = 0;
-	
+	tmp_dest = dest;
+	tmp_src = src;
+	if (dest < src)
+		while (i < n)
+		{
+			tmp_dest[i] = tmp_src[i];
+			i++;
+			printf("Y");
+		}
+	else
+		while (n)
+		{
+			*(tmp_dest + n) = *(tmp_src + n);
+			n--;
+			printf("N");
+		}
+	return (dest);
 }
