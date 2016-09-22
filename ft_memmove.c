d7370ed8913202e02acd9c9aaf5f3a5081bc83ba
Wazabiii42 <fourcade.adri2@gmail.com>
2016-09-22 16:02:13 +0200

    rename with ft

---
+++ ft_memmove.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	*memmove(void *dest, const void *src, size_t n)
+{
+	int		*tmp;
+	unsigned int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		tmp[i] = src[i];
+		i++;
+	}
+	i = 0;
+	
+}
