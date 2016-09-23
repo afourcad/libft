57eaf813236943f359ca8733e8481ad364812a63
Wazabiii42 <fourcade.adri2@gmail.com>
2016-09-23 16:19:37 +0200

    add usefull fonctions

---
+++ ft_putstr.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+void	ft_putstr(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
--- libft.h
+++ libft.h
@@ -4,11 +4,14 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <string.h>
+# include <unistd.h>
 
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
 
 # endif
